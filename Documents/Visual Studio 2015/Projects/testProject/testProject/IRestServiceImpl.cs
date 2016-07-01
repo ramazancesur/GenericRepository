40375b419e6da95ea1319dae312fee44028d68d2
ramazancesur <ramazancesur3@gmail.com>
2016-07-01 16:17:17 +0300

    project Sending

---
+++ Documents/Visual Studio 2015/Projects/testProject/testProject/IRestServiceImpl.cs
@@ -0,0 +1,18 @@
+﻿using System.ServiceModel;
+using System.ServiceModel.Web;
+using testProject.Yetkiler;
+
+namespace testProject
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IRestServiceImpl" in both code and config file together.
+    [ServiceContract]
+    public interface IRestServiceImpl
+    {
+        [OperationContract]
+        [WebInvoke(Method = "GET",
+                    ResponseFormat = WebMessageFormat.Json,
+                    UriTemplate = "yetkiler/{username}/{passwd}")]
+        KullaniciYetkiBilgisi yetkiBilgisi(string username,string passwd);
+
+    }
+}
---
+++ Documents/Visual Studio 2015/Projects/testProject/testProject/RestServiceImpl.svc.cs
@@ -0,0 +1,19 @@
+﻿using testProject.Yetkiler;
+
+namespace testProject
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "RestServiceImpl" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select RestServiceImpl.svc or RestServiceImpl.svc.cs at the Solution Explorer and start debugging.
+    public class RestServiceImpl : IRestServiceImpl
+    {
+        public KullaniciYetkiBilgisi yetkiBilgisi(string username, string passwd)
+        {
+            Yetkiler.YetkiServiceClient yetki = new Yetkiler.YetkiServiceClient();
+            yetki.ClientCredentials.UserName.UserName = username;
+            yetki.ClientCredentials.UserName.Password = passwd;
+
+            KullaniciYetkiBilgisi yetkiler = yetki.KullaniciYetkileriniGetir(null, null, true, null, null, null, null);
+            return yetkiler;
+        }
+    }
+}
