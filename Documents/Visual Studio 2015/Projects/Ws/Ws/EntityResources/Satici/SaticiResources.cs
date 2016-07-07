6b58495ea98eb38bc46542722cc94e3becdc84f3
ramazancesur <ew>
2016-07-07 14:54:11 +0300

    rc commit 9

--- Documents/Visual Studio 2015/Projects/Ws/Ws/EntityResources/Satici/SaticiResources.cs
+++ Documents/Visual Studio 2015/Projects/Ws/Ws/EntityResources/Satici/SaticiResources.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using test2.EntityDao.Address;
 using test2.EntityDao.Contact;
@@ -32,7 +33,109 @@ namespace Ws.EntityResources.Satici
             roleDao = new RoleDao();
             contactDao = new ContactDao();
         }
+        public SaticiDTO deleteSatici(SaticiDTO satici, CalisanDTO calisan)
+        {
+            // ********* address deleted ******** //
+            List<address> lstAddress = addresDao.FindBy(x => x.address1 == satici.address
+                                               && x.isActive == 1).ToList();
+            foreach (var address in lstAddress )
+            {
+                address.updateDate = DateTime.Now;
+                address.isActive = 0;
+                addresDao.Edit(address);
+                createLog.SaveLog(calisan, address, 0);
+            }
+            // ********** Firm deleted ************** //
+            List<firm> lstFirm = firmDao.FindBy(x => x.name == satici.campanyName && x.isActive == 1).ToList();
+            foreach (var firm in lstFirm)
+            {
+                firm.updateDate = DateTime.Now;
+                firm.isActive = 0;
+                firmDao.Edit(firm);
+                createLog.SaveLog(calisan, firm, 0);
+            }
+            // ********* Employee deleted ********* //
+            List<employee> lstEmployee = employeeDao.FindBy(x => x.title == satici.name && x.creationDate.Value.ToShortTimeString() == satici.createTime.ToShortTimeString()
+                           && x.isActive == 1).ToList();
+            foreach (var emp in lstEmployee)
+            {
+                emp.isActive = 0;
+                emp.updateDate = DateTime.Now;
+                employeeDao.Edit(emp);
+                createLog.SaveLog(calisan, emp, 0);
+            }
+            // ****** Contact deleted ******** //
+            List<contact> lstContact = contactDao.FindBy(x => x.value == satici.phoneNumber).ToList();
+            foreach (var contact in lstContact)
+            {
+                contact.isActive = 0;
+                contact.updateDate = DateTime.Now;
+                contactDao.Edit(contact);
+                createLog.SaveLog(calisan, contact, 0);
+            }
+            return satici;
+        }
+        #region updateSatici
+        public SaticiDTO updateSatici(SaticiDTO satici, CalisanDTO calisan)
+        {
+            // ********* address updated ******** //
+            address address = addresDao.FindBy(x => x.creationDate.Value.ToShortTimeString() == satici.createTime.ToShortTimeString()
+                                               && x.isActive == 1).FirstOrDefault();
+            address.address1 = satici.address;
+            address.updateDate = DateTime.Now;
+            addresDao.Edit(address);
+            createLog.SaveLog(calisan, address, 2);
+            // ********** Firm Updated ************** //
+            List<firm> lstFirm = firmDao.FindBy(x => x.name == satici.campanyName && x.isActive == 1).ToList();
+            foreach (var firm in lstFirm)
+            {
+                firm.updateDate = DateTime.Now;
+                firm.isActive = satici.isActive;
+                firm.logo = satici.companyLogoPath;
+                firmDao.Add(firm);
+                createLog.SaveLog(calisan, firm, 2);
+            }
+            // ********* Employee Updated ********* //
+            List<employee> lstEmployee = employeeDao.FindBy(x => x.title == satici.name && x.creationDate.Value.ToShortTimeString() == satici.createTime.ToShortTimeString()
+                             && x.isActive == 1).ToList();
+            foreach (var emp in lstEmployee)
+            {
+                try
+                {
+                    emp.firmID = lstFirm[0].id;
+                    emp.isActive = 1;
+                    emp.updateDate = DateTime.Now;
+                    emp.username = satici.sallerName;
+                    emp.title = satici.name;
+                    emp.password = satici.passwd;
+                    try
+                    {
+                        emp.roleID = roleDao.FindBy(x => x.name == satici.roleName && x.isActive == 1).LastOrDefault().id;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                    }
+                    employeeDao.Add(emp);
+                    createLog.SaveLog(calisan, emp, 2);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            // ****** Contact Insertion ******** //
+            contact contact = contactDao.FindBy(x => x.value == satici.phoneNumber).FirstOrDefault();
+            contact.contactType = (int)satici.contactType;
+            contact.updateDate = DateTime.Now;
+            contactDao.Edit(contact);
+            createLog.SaveLog(calisan, contact, 2);
 
+            return satici;
+        }
+        #endregion
+
+        #region createSatici
         public SaticiDTO createSatici(SaticiDTO satici, CalisanDTO calisan)
         {
             // ********* address adding ******** //
@@ -55,7 +158,7 @@ namespace Ws.EntityResources.Satici
             firmDao.Add(firm);
             createLog.SaveLog(calisan, firm, 1);
 
-            // ********* Emloyee Insertion ********* //
+            // ********* Employee Insertion ********* //
             employee emp = new employee();
             emp.firmID = firm.id;
             emp.isActive = 1;
@@ -74,7 +177,7 @@ namespace Ws.EntityResources.Satici
             }
             employeeDao.Add(emp);
             createLog.SaveLog(calisan, emp, 1);
-
+            // ****** Contact Insertion ******** //
             contact contact = new contact();
             contact.creationDate = DateTime.Now;
             contact.updateDate = DateTime.Now;
@@ -86,5 +189,6 @@ namespace Ws.EntityResources.Satici
 
             return satici;
         }
+        #endregion
     }
 }
